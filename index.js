04cfbb39b37ba0b7581b5a44035043732b9ef157
akumq <darkshein65@gmail.com>
2022-03-19 00:47:41 +0100

    commit

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require("path");
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user.routes');
-require('dotenv').config({path: './config/.env'});
+require('dotenv').config({ path: './config/.env' });
 const app = express();
 
-app.use("/static",express.static(path.resolve(__dirname,"frontend","static")))
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")))
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
@@ -23,6 +23,6 @@ app.listen(process.env.PORT, () => {
 
 // index.html
 
-app.get("/*",(req,res) => {
-    res.sendFile(path.resolve(__dirname, "frontend","index.html"));
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
